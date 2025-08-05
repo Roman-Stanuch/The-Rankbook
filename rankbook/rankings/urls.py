28bae4304532897e53c44f53a10df1323c32d77f
Polygonal Graphics <154336865+Roman-Stanuch@users.noreply.github.com>
2025-08-05 09:09:56 -0500

    Added Basic Form Functionality
    Added the ability to submit a form that adds the data to a list on the rankings home page. Data is currently stored in cookies and separate for each user.

--- rankbook/rankings/urls.py
+++ rankbook/rankings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "rankings"
 urlpatterns = [
     path("", views.index, name="index"),
     path("submit/", views.submit, name="submit")
--- rankbook/rankings/views.py
+++ rankbook/rankings/views.py
@@ -1,15 +1,39 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 import datetime
 
+class NewVoteForm(forms.Form):
+    school = forms.CharField(label="School")
+    business = forms.CharField(label="Business")
+    type = forms.CharField(label="Type")
+
 # Create your views here.
 def index(request):
-    now = datetime.datetime.now()
-    rankings = ["Cornell", "Madison", "Carnegie"]
+    if "rankings" not in request.session:
+        request.session["rankings"] = []
+
     return render(request, "rankings/index.html", {
-        "date": now.date(),
-        "rankings": rankings
+        "rankings": request.session["rankings"] 
     }) 
 
 def submit(request):
-    return render(request, "rankings/submit.html")
+    if request.method == "POST":
+        form = NewVoteForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            school = data["school"]
+            business = data["business"]
+            type = data["type"]
+            request.session["rankings"] += [school + " with " + type + " from " + business]
+            return HttpResponseRedirect(reverse("rankings:index"))
+        else:
+            return render(request, "rankings/submit.html", {
+                "form": form 
+            })
+
+    return render(request, "rankings/submit.html", {
+        "form": NewVoteForm()
+    })
